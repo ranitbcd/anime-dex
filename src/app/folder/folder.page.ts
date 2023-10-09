424fb40202aa40235e261993f3c777829571124b
Ranit Mondal <ranit.mondal.bcd@outlook.com>
2023-10-09 18:39:52 +0530

    next commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AnimeService } from 'src/services/anime.service';
 
 @Component({
   selector: 'app-folder',
@@ -9,9 +10,14 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor(private _animeService : AnimeService) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+  async hitApi(){
+    await this._animeService.getAllAnimeDetails().subscribe(res=>{
+      console.log(res)
+    })
   }
 }
